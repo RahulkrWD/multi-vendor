aa12c0604dd1eba4b4e42cef2c40662d0057bcb0
RahulkrWD <rahulkmrgaya21@gmail.com>
2025-03-19 17:19:37 +0530

    navbar loading

--- js/utils.js
+++ js/utils.js
@@ -6,41 +6,24 @@ export function showMessage(color, message) {
   }
 }
 
-// export function showLoadingBar() {
-// const loadingBar = document.getElementById("loading-bar");
-// if (loadingBar) {
-//   loadingBar.style.width = "0%";
-//   loadingBar.style.display = "block";
-
-//   setTimeout(() => {
-//     loadingBar.style.width = "100%";
-//   }, 0);
-// }
-// }
-
-// export function hideLoadingBar() {
-//   const loadingBar = document.getElementById("loading-bar");
-//   if (loadingBar) {
-//     setTimeout(() => {
-//       loadingBar.style.width = "0%";
-//       loadingBar.style.display = "none";
-//     }, 1000);
-//   }
-// }
-// export function showLoadingBar() {
-//   document.getElementById("loading-circle").style.display = "block";
-// }
-
-// export function hideLoadingBar() {
-//   document.getElementById("loading-circle").style.display = "none";
-// }
-
-// Show the loading spinner
 export function showLoadingBar() {
-  document.getElementById("loadingOverlay").style.display = "flex";
+  const loadingBar = document.getElementById("loading-bar");
+  if (loadingBar) {
+    loadingBar.style.width = "0%";
+    loadingBar.style.display = "block";
+
+    setTimeout(() => {
+      loadingBar.style.width = "100%";
+    }, 0);
+  }
 }
 
-// Hide the loading spinner
 export function hideLoadingBar() {
-  document.getElementById("loadingOverlay").style.display = "none";
+  const loadingBar = document.getElementById("loading-bar");
+  if (loadingBar) {
+    setTimeout(() => {
+      loadingBar.style.width = "0%";
+      loadingBar.style.display = "none";
+    }, 1000);
+  }
 }
