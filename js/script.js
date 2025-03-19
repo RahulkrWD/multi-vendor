35b1264499a73aac6cbfc92eda542ab263903509
RahulkrWD <rahulkmrgaya21@gmail.com>
2025-03-19 15:49:16 +0530

    fix navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 import { checkUserLogin } from "./user.js";
 import { CreateUser, LoginUser } from "./auth.js";
 
+async function loadNavbar(id, file) {
+  let response = await fetch(file);
+  let data = await response.text();
+  document.getElementById(id).innerHTML = data;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   checkUserLogin();
+  loadNavbar("navbar", "./components/navbar.html");
 
   document
     .getElementById("registerForm")
